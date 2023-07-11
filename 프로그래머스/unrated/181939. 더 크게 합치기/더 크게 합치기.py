3d936342750d39203524c0f1f812e86defc6554c
kjjh714 <59947483+kjjh714@users.noreply.github.com>
2023-07-11 18:36:37 +0900

    Update 더 크게 합치기.py

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,12 +1,16 @@
+# def solution(a, b):
+#     ab = str(a) + str(b)
+#     ba = str(b) + str(a)
+    
+#     if ab == ba:
+#         result = int(ab)
+#     elif ab > ba:
+#         result = int(ab)
+#     else:
+#         result = int(ba)
+    
+#     return result
+
 def solution(a, b):
-    ab = str(a) + str(b)
-    ba = str(b) + str(a)
-    
-    if ab == ba:
-        result = int(ab)
-    elif ab > ba:
-        result = int(ab)
-    else:
-        result = int(ba)
-    
+    result = max(int(str(a)+str(b)), int(str(b)+str(a)))
     return result
