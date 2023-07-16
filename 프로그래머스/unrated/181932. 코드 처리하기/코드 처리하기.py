fb1ce0900f145bfab053eedb34ab8429cb0f1d8e
kjjh714 <59947483+kjjh714@users.noreply.github.com>
2023-07-16 20:51:03 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,19 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    for idx in range(len(code)):
+        if mode==0:
+            if code[idx] != '1':
+                if idx % 2 == 0 :
+                    ret += code[idx]
+            else :
+                mode = 1
+        else :
+            if code[idx] != '1':
+                if idx % 2 == 1:
+                    ret += code[idx]
+            else:
+                mode = 0
+    if len(ret) == 0: ret = 'EMPTY'
+    
+    return ret
