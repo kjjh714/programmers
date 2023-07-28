ee660bc9f3bcc7443b4335c67d86904db9ee737e
kjjh714 <59947483+kjjh714@users.noreply.github.com>
2023-07-28 18:31:46 +0900

    Update 문자열 여러 번 뒤집기.py

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -6,3 +6,10 @@ def solution(my_string, queries):
         answer = ''.join(my_string_list)
         
     return answer
+
+
+# 다른 사람 풀이
+def solution(my_string, queries):
+    for s,e in queres:
+        my_string = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    return my_string
