3a31d19bc9d5562267c32e241414786d0dc53b53
Kim Ji Hyeon <59947483+kjjh714@users.noreply.github.com>
2025-03-31 16:44:35 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,13 @@
 -- 코드를 입력하세요
-select to_number(to_char(datetime, 'hh24')) hour, count(*) as count
+select hour(datetime) as hour, count(*) as count
 from animal_outs
-where to_char(datetime, 'hh24') between 09 and 19
-group by to_char(datetime, 'hh24')
-order by hour 
+group by hour
+having hour between 9 and 19
+order by hour
+
+-- ver.oracle
+# select to_number(to_char(datetime, 'hh24')) hour, count(*) as count
+# from animal_outs
+# where to_char(datetime, 'hh24') between 09 and 19
+# group by to_char(datetime, 'hh24')
+# order by hour
